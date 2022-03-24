122726854d2c3673e597756e3fc95c48fedf4da4
Daxonus <elias@dobrin.net>
2022-03-24 11:34:23 -0700

    added some building code

--- building.py
+++ building.py
@@ -13,6 +13,13 @@ class System:
         self.nodes = nodes
         self.links = links
 
+    def __str__(self) -> str:
+        return f"Nodes {self.nodes}\nLinks {self.links}"
+
+    def iterate(self, delta_time: float) -> None:
+        for node in self.nodes:
+            node.iterate(delta_time)
+
 
 class Node:
     mass: float
@@ -27,6 +34,17 @@ class Node:
         self.velocity = velocity
         self.acceleration = acceleration
         self.force = force
+
+    def __str__(self) -> str:
+        return "Node"
+
+    def update_acceleration(self) -> None:
+        self.acceleration = self.force / self.mass
+
+    def iterate(self, delta_time) -> None:
+        self.update_acceleration()
+        self.velocity += self.acceleration * delta_time
+        self.position += self.velocity * delta_time
 
 
 class Link:
@@ -45,6 +63,12 @@ class Link:
         self.dampening = dampening
         self.actual_speed = actual_speed
 
+    def __str__(self) -> str:
+        return f"{self.nodes[0].position} <-> {self.nodes[1].position}"
+
+    def get_unit_vector(self) -> Point:
+        return (self.nodes[1].position - self.nodes[0].position) / self.resting_distance
+
     def get_displacement(self) -> float:
         return self.actual_distance - self.resting_distance
 
@@ -57,8 +81,37 @@ class Link:
     def get_spring_force(self) -> float:
         return self.get_stiffness_force() + self.get_dampening_force()
 
-    def update_actual_distance(self) -> None:
+    def update_actual_distance(self) -> float:
+        previous_distance = self.actual_distance
         self.actual_distance = Point.dist(self.nodes[0].position, self.nodes[1].position)
+        return previous_distance
 
-    def update_actual_speed(self, previous_distance: float, delta_time: float) -> None:
+    def update_actual_speed(self, delta_time: float) -> None:
+        previous_distance = self.update_actual_distance()
         self.actual_speed = (self.actual_distance - previous_distance) / delta_time
+
+    def apply_forces(self) -> None:
+        self.nodes[0].force += self.get_spring_force() * self.get_unit_vector()
+        self.nodes[1].force -= self.get_spring_force() * self.get_unit_vector()
+
+
+length: int = 10
+width: int = 5
+height: int = 20
+
+nodes = [[[Node(mass=1, position=Point(0, 0, 0), velocity=Point(0, 0, 0), acceleration=Point(0, 0, 0), force=Point(0, 0, 0))
+         for z in range(height)] for y in range(width)] for x in range(length)]
+links = []
+
+for x in range(1, length - 2):
+    for y in range(1, width - 1):
+        for z in range(1, height - 1):
+            links.append(Link(nodes=(nodes[x][y][z], nodes[x + 1][y][z]),
+                              resting_distance=1,
+                              actual_distance=1,
+                              stiffness=1,
+                              dampening=1,
+                              actual_speed=0))
+
+system = System(nodes=[nodes[x][y][z] for x in range(length) for y in range(width) for z in range(height)], links=links)
+print(system.links[0])
