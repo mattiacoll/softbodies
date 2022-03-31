406ad8840a3022ca9981c8f792c66881e6048fc0
Daxonus <elias@dobrin.net>
2022-03-31 12:04:31 -0700

    lots of docs

--- softbodies.py
+++ softbodies.py
@@ -3,6 +3,7 @@ from points import Point
 
 
 class Softbody:
+    """A spring-network type softbody consisting of nodes (point masses) and links (Hookean springs)."""
     nodes: list[Node]
     links: list[Link]
 
@@ -10,12 +11,22 @@ class Softbody:
         self.nodes = nodes
         self.links = links
 
+    def apply_link_forces(self) -> None:
+        """Apply the force of each link on each of the corresponding nodes."""
+        for link in self.links:
+            link.apply_force()
+
+    def iterate(self, delta_time) -> None:
+        """Integrate the position and velocity of each node with Euler's method."""
+        for node in self.nodes:
+            node.iterate(delta_time)
+
 
 class Node:
+    """A point mass particle that implements Euler integration."""
     mass: float
     position: Point
     velocity: Point
-    acceleration: Point
     force: Point
 
     def __init__(self, mass: float, position: Point) -> None:
@@ -23,10 +34,21 @@ class Node:
         self.position = position
 
     def apply_force(self, force: Point) -> None:
+        """Add a singular force to the node."""
         self.force += force
+
+    def reset_force(self) -> None:
+        """Reset the force applied on the node."""
+        self.force = Point(0, 0)
+
+    def iterate(self, delta_time: float) -> None:
+        """Integrate the position and velocity with Euler's method."""
+        self.velocity += (self.force / self.mass) * delta_time
+        self.position += self.velocity * delta_time
 
 
 class Link:
+    """A massless Hookean spring that features a stiffness force and a spring force."""
     nodes: tuple[Node, Node]
     resting_length: float
     stiffness: float
@@ -39,24 +61,31 @@ class Link:
         self.dampening = dampening
 
     def get_length(self) -> float:
+        """Get the momentary length of the link."""
         return Point.dist(self.nodes[0].position, self.nodes[1].position)
 
     def get_speed(self) -> float:
+        """Get the speed of the expansion/contraction of the link (positive/negative)."""
         return Point.dot(self.nodes[0].position - self.nodes[1].position, self.nodes[0].velocity - self.nodes[1].velocity) / self.get_length()
 
     def get_displacement(self) -> float:
+        """Get the expansion/contraction of the link from its resting configuration (positive/negative)."""
         return self.get_length() - self.resting_length
 
     def get_stiffness_force(self) -> float:
+        """Get the spring stiffness force expansion/contraction (positive/negative)."""
         return -self.stiffness * self.get_displacement()
 
     def get_dampening_force(self) -> float:
+        """Get the spring dampening force expansion/contraction (positive/negative)."""
         return -self.dampening * self.get_speed()
 
     def get_force(self) -> float:
+        """Get the spring force expansion/contraction (positive/negative)."""
         return self.get_stiffness_force() + self.get_dampening_force()
 
     def apply_force(self) -> None:
-        self.nodes[0].apply_force(self.get_spring_force() * ((self.nodes[0].position - self.nodes[1].position) / self.get_length()))
-        self.nodes[1].apply_force(self.get_spring_force() * ((self.nodes[1].position - self.nodes[0].position) / self.get_length()))
+        """Apply the pair of opposite facing spring forces on the pair of nodes."""
+        self.nodes[0].apply_force(self.get_force() * ((self.nodes[0].position - self.nodes[1].position) / self.get_length()))
+        self.nodes[1].apply_force(self.get_force() * ((self.nodes[1].position - self.nodes[0].position) / self.get_length()))
 
