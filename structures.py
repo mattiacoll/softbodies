b4d4d62f3171976aeeaa039de0cb638ae4e38813
Daxonus <elias@dobrin.net>
2022-04-01 21:18:39 -0700

    finished pyramid structure

--- structures.py
+++ structures.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
+import numpy as np
 from softbodies import Softbody, Node, Link, Point
 
 
 class Tower(Softbody):
-    def __init__(self, position: Point, size: tuple[float, float], grid: tuple[int, int], mass: float, stiffness: float, dampening: float) -> None:
+    def __init__(self, position: Point, size: tuple[float, float], grid: tuple[int, int], mass: float, stiffness: float,
+                 dampening: float) -> None:
         nodes = []
         for x in range(grid[0] + 1):
             nodes.append([])
@@ -28,5 +30,27 @@ class Tower(Softbody):
 
 
 class Pyramid(Softbody):
-    def __init__(self, position: Point, size: tuple[float, float], grid: int, mass: float, stiffness: float, dampening: float) -> None:
+    def __init__(self, position: Point, size: tuple[float, float], grid: int, mass: float, stiffness: float,
+                 dampening: float) -> None:
         nodes = []
+        for y in range(grid + 1):
+            nodes.append([])
+            for x in range(grid + 1 - y):
+                nodes[y].append(Node(mass=mass / ((grid + 1) * (grid + 2) / 2),
+                                     position=Point(position.x - size[0] / 2 + x,
+                                                    position.y - size[1] / 2 + y)))
+
+        links = []
+        for y in range(grid + 1):
+            for x in range(grid - y):
+                links.append(Link(nodes=(nodes[y][x], nodes[y][x + 1]), stiffness=stiffness, dampening=dampening))
+
+        for y in range(grid):
+            for x in range(grid - y):
+                links.append(Link(nodes=(nodes[y][x], nodes[y + 1][x]), stiffness=stiffness, dampening=dampening))
+
+        for y in range(grid):
+            for x in range(grid - y):
+                links.append(Link(nodes=(nodes[y][x + 1], nodes[y + 1][x]), stiffness=stiffness, dampening=dampening))
+
+        super().__init__(nodes=[node for buffer in nodes for node in buffer], links=links)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from time import sleep
 from scipy.constants import g
 import pygame
 from softbodies import Softbody, Node, Link, Point
-from structures import Tower
+from structures import Tower, Pyramid
 
 nodes = [Node(mass=1, position=Point(1, 1)),
          Node(mass=1, position=Point(1, -1)),
@@ -14,10 +14,10 @@ links = [Link(nodes=(nodes[0], nodes[1]), stiffness=100, dampening=1),
          Link(nodes=(nodes[3], nodes[0]), stiffness=100, dampening=1),
          Link(nodes=(nodes[0], nodes[2]), stiffness=100, dampening=1, resting_length=2),
          Link(nodes=(nodes[1], nodes[3]), stiffness=100, dampening=1, resting_length=2)]
-softbody = Softbody(nodes=nodes, links=links)
-softbody = Tower(position=Point(0, 0), size=(2, 2), grid=(2, 2), mass=1, stiffness=50, dampening=0)
-
-
+#softbody = Softbody(nodes=nodes, links=links)
+#softbody = Tower(position=Point(0, 0), size=(4, 4), grid=(7, 7), mass=1, stiffness=50, dampening=0)
+softbody = Pyramid(position=Point(0, 0), size=(2, 2), grid=7, mass=1, stiffness=1000, dampening=0)
+print(len(softbody.links))
 camera_position = Point(0, 0)
 camera_zoom = 0.2
 
