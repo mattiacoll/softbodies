b78f2928d8021dcdaab4b6f7ae35ad1d3586483c
Daxonus <elias@dobrin.net>
2022-04-16 15:42:25 -0700

    better structure :-)

--- structures.py
+++ structures.py
@@ -1,19 +1,33 @@
 from __future__ import annotations
-from math import cos, sin, tau, sqrt
+from math import sqrt, cos, sin, atan2, tau
 from random import random
-from softbodies import Node, Link, Point
+from typing import NewType
+from softbodies import Node, Link
+from points import Point
 
-softbody = tuple[list[Node], list[Link]]
+Softbody = tuple[list[Node], list[Link]]
 
 
-def tower(position: Point, size: tuple[float, float], grid: tuple[int, int], mass: float, stiffness: float, dampening: float) -> softbody:
+def structure(softbody: Softbody, position: Point, scale: float, rotation: float) -> Softbody:
+    nodes, links = softbody
+    for node in nodes:
+        node_angle = atan2(node.position.y, node.position.x)
+        node_radius = node.position.dist(Point(0, 0))
+        node.position.x = node_radius * cos(node_angle + rotation)
+        node.position.y = node_radius * sin(node_angle + rotation)
+        node.position *= scale
+        node.position += position
+    return nodes, links
+
+
+def tower(position: Point, size: tuple[float, float], grid: tuple[int, int], mass: float, stiffness: float, dampening: float) -> Softbody:
     nodes_mesh = []
     for x in range(grid[0] + 1):
         nodes_mesh.append([])
         for y in range(grid[1] + 1):
             node_mass = mass / ((grid[0] + 1) * (grid[1] + 1))
-            node_position = Point(position.x + size[0] * (x / grid[0] - 0.5),
-                                  position.y + size[1] * (y / grid[1] - 0.5))
+            node_position = Point(size[0] * (x / grid[0] - 0.5),
+                                  size[1] * (y / grid[1] - 0.5))
             nodes_mesh[x].append(Node(node_mass, node_position))
     links = []
     for x in range(grid[0]):
@@ -29,16 +43,16 @@ def tower(position: Point, size: tuple[float, float], grid: tuple[int, int], mas
         for y in range(grid[1]):
             links.append(Link((nodes_mesh[x + 1][y], nodes_mesh[x][y + 1]), stiffness, dampening))
     nodes = [node for buffer in nodes_mesh for node in buffer]
-    return nodes, links
+    return structure((nodes, links), position, 1, 0)
 
 
-def pyramid(position: Point, size: tuple[float, float], grid: int, mass: float, stiffness: float, dampening: float) -> softbody:
+def pyramid(position: Point, size: tuple[float, float], grid: int, mass: float, stiffness: float, dampening: float) -> Softbody:
     nodes_mesh = []
     for y in range(grid + 1):
         nodes_mesh.append([])
         for x in range(grid + 1 - y):
             node_mass = mass / ((grid + 1) * (grid + 2) / 2)
-            node_position = Point(position.x - size[0] / 2 + x, position.y - size[1] / 2 + y)
+            node_position = Point(-size[0] / 2 + x, -size[1] / 2 + y)
             nodes_mesh[y].append(Node(node_mass, node_position))
     links = []
     for y in range(grid + 1):
@@ -51,10 +65,10 @@ def pyramid(position: Point, size: tuple[float, float], grid: int, mass: float, 
         for x in range(grid - y):
             links.append(Link((nodes_mesh[y][x + 1], nodes_mesh[y + 1][x]), stiffness, dampening))
     nodes = [node for buffer in nodes_mesh for node in buffer]
-    return nodes, links
+    return structure((nodes, links), position, rotation=0, scale=1)
 
 
-def blob(position: Point, size: float) -> softbody:
+def blob(position: Point, size: float) -> Softbody:
     nodes = []
     for n in range(30):
         radius = (size / 2) * sqrt(random())
@@ -68,4 +82,4 @@ def blob(position: Point, size: float) -> softbody:
 
             if Point.dist(node_1.position, node_2.position) <= size / 3:
                 links.append(Link(nodes=(node_1, node_2), stiffness=200, dampening=0))
-    return nodes, links
+    return structure((nodes, links), position, rotation=0, scale=1)
