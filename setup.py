f4c4e6cce8949954f536b50f6f1a2823aafb2adb
Elias Dobrin <elias@dobrin.net>
2022-06-13 10:33:33 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="softbodies",
+    version="1.0.0",
+    description="Module for simulating physics of softbodies following Hooke's principles.",
+    author="Elias Dobrin",
+    url="https://github.com/Elixonus/softbodies",
+    license="MIT",
+    keywords=["softbody", "spring", "springs"]
+)
